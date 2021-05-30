4cdcca015c8b6f623378abfe11d180704371b46f
Hashim Sohail <hashimsohail21@gmail.com>
2021-05-30 23:38:24 +0500

    redis provider debug

--- lib/redis_provider.ex
+++ lib/redis_provider.ex
@@ -22,19 +22,20 @@ defmodule AbsintheCache.RedisProvider do
   end
 
   @impl true
-  def get(_, key) do
+  def get(_, {key, _}) do
     {:ok, conn} = Redix.start_link(System.get_env("REDIS_URL", "redis://127.0.0.1:6379"))
-    case Redix.command(conn, ["GET", key]) do
+
+    case Redix.command(conn, ["GET", key]) |> IO.inspect() do
       {:ok, value} ->
         value
+
       _ ->
         nil
     end
-
   end
 
   @impl true
-  def store(cache, key, value) do
+  def store(cache, {key, _}, value) do
     case value do
       {:error, _} ->
         :ok
@@ -49,15 +50,22 @@ defmodule AbsintheCache.RedisProvider do
   end
 
   @impl true
-  def get_or_store(cache, key, func, cache_modify_middleware) do
+  def get_or_store(cache, {key, _}, func, cache_modify_middleware) do
+    IO.puts("IN GET OR STORE")
+    IO.inspect(cache)
+    IO.inspect(key)
     {:ok, conn} = Redix.start_link(System.get_env("REDIS_URL", "redis://127.0.0.1:6379"))
+
     {result, error_if_any} =
-      case Redix.command(conn, ["GET", key]) do
-        {:ok, value} ->
-          {value, nil}
+      case Redix.command(conn, ["GET", key]) |> IO.inspect() do
+        {:ok, value} when value != nil ->
+          IO.puts("VALUE HAI")
+          IO.inspect(VALUE)
+          value = Jason.decode!(value)
+          {{:ok, value}, nil}
 
         _ ->
-          case func.() do
+          case func.() |> IO.inspect() do
             {:error, _} = error ->
               {nil, error}
 
@@ -69,11 +77,16 @@ defmodule AbsintheCache.RedisProvider do
               Process.put(:do_not_cache_query, true)
               {value, nil}
 
-            value ->
-              cache_item(cache, key, value)
-              {value, nil}
+            {:ok, value} ->
+              IO.puts("VALUE HAI, CACHE ADD KAROu")
+              cache_item(cache, key, Jason.encode!(value)) |> IO.inspect()
+              {{:ok, value}, nil}
           end
       end
+
+    IO.puts("IN END")
+    IO.inspect(result)
+    IO.inspect(error_if_any)
 
     if error_if_any != nil do
       # Logger.warn("Somethting went wrong...")
@@ -84,12 +97,17 @@ defmodule AbsintheCache.RedisProvider do
   end
 
   defp cache_item(_, {_, ttl} = key, value) when is_integer(ttl) and ttl <= @max_cache_ttl do
+    IO.puts("IN CACHE ITEM 1")
+    IO.inspect(key)
+    IO.inspect(value)
     {:ok, conn} = Redix.start_link(System.get_env("REDIS_URL", "redis://127.0.0.1:6379"))
     Redix.command(conn, ["SET", key, value])
-
   end
 
   defp cache_item(_, key, value) do
+    IO.puts("IN CACHE ITEM")
+    IO.inspect(key)
+    IO.inspect(value)
     {:ok, conn} = Redix.start_link(System.get_env("REDIS_URL", "redis://127.0.0.1:6379"))
     Redix.command(conn, ["SET", key, value])
   end
